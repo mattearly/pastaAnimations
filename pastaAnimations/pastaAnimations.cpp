7e6f76e395840deffa6f4f9b9bd8eb11166f7134
Matthew Early <matthewjearly@gmail.com>
2021-10-04 19:06:42 -0400

    adjust starting pos and move speed

--- pastaAnimations/pastaAnimations.cpp
+++ pastaAnimations/pastaAnimations.cpp
@@ -34,7 +34,7 @@ enum Camera_Movement {
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 12.5f;
+const float SPEED = 7.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 60.0f;
 
@@ -1015,7 +1015,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 1.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -1121,7 +1121,7 @@ int main()
     // render the loaded model
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(0.0f, -0.4f, 0.0f)); // translate it down so it's at the center of the scene
-    model = glm::scale(model, glm::vec3(.5f, .5f, .5f));	// it's a bit too big for our scene, so scale it down
+    model = glm::scale(model, glm::vec3(1.f));	// it's a bit too big for our scene, so scale it down
     ourShader.setMat4("model", model);
     ourModel.Draw(ourShader);
 
